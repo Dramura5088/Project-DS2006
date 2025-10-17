4452d22cc002aaa279f27c4e3c5945b3cfa68830
Philip <philling_good@hotmail.com>
2025-10-17 14:25:46 +0200

    fixed a small bug

--- src/data.py
+++ src/data.py
@@ -1,15 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-class Data():
-    def __init__(self, data:pd.DataFrame):
+
+class Data:
+    def __init__(self, data: pd.DataFrame):
         self.df = data
         self.features = self.df.drop("class")
         self.classes = self.df("class")
 
         self.normalSplit()
-    
-    def normalSplit(self):
-        features_train, features_test, classes_train, classes_test = self.train_test_split(
-            self.features, self.classes, test_size=0.2, random_state=10)
 
+    def normalSplit(self):
+        (
+            self.features_train,
+            self.features_test,
+            self.classes_train,
+            self.classes_test,
+        ) = self.train_test_split(
+            self.features, self.classes, test_size=0.2, random_state=10
+        )
