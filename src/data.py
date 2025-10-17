24b335db7753bdbb168a500ab4c6a2533d592128
Philip <philling_good@hotmail.com>
2025-10-17 18:38:11 +0200

    fixed a critical error where i didnt reference data correctly. Rip 2h...

--- src/data.py
+++ src/data.py
@@ -5,17 +5,24 @@ from sklearn.model_selection import train_test_split
 class Data:
     def __init__(self, data: pd.DataFrame):
         self.df = data
-        self.features = self.df.drop("Mine_Type")
-        self.classes = self.df("Mine_Type")
+        self.features = self.df.drop('Mine_Type', axis=1)
+        self.classes = self.df['Mine_Type']
 
-        self.normalSplit()
+        
+    def manualSplit(self, trainingData:pd.DataFrame):
+        pass
 
-    def normalSplit(self):
+    def normalSplit(self, stratify = False ,random_state = 10):
         (
             self.features_train,
             self.features_test,
             self.classes_train,
             self.classes_test,
         ) = self.train_test_split(
-            self.features, self.classes, test_size=0.2, random_state=10
+            self.features, self.classes, test_size=0.4, random_state=random_state, shuffle=True, stratify=stratify 
         )
+    
+    def evaluate(self):
+        print("Head:\n" + self.df.head(10)) # Loads first 10 lines (requirement in 3.a)
+        print("Describe:\n" + self.df.describe())
+        print("Shape:\n" + self.df.shape)
