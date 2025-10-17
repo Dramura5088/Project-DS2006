a0efe41b2228072734861142232a301afa6294d6
RonniEvrensel <ronni94@live.se>
2025-10-17 17:57:26 +0200

    in progress for userchoice

--- src/main.py
+++ src/main.py
@@ -32,33 +32,62 @@ def yesOrNo(statement: str) -> bool:
         return True
     else:
         return False
-
-
-def main():
+def loadFile() -> pd.DataFrame:
     fileNotFound: bool = False
     doILoadBaseFile: bool = yesOrNo("Do you want to load the standard Dataset?")
 
     # Load base file
+    df:pd.DataFrame
     if doILoadBaseFile == True:
         try:
             df = pd.read_csv("Mine_Dataset.csv")
+            return df
         except:
             fileNotFound = True
             print("Base file not found. Switching to user selection.")
     # Let the user load a file
     elif fileNotFound or doILoadBaseFile == False:
-        while True:
+        while True: # While loop to get correct input.
             try:
                 filename = input("Type Dataset filename to load:")
                 df = pd.read_csv(filename)
-                break
+                return df
             except:
                 print("File not found or data was innacurate.")
 
+
+def main():
+    df = loadFile()
+    #print(df.info())
     data: Data = Data(df)
     
 
     # Ronni from here
+    from classifiers import Classifier_kNN, Classifier_Decision_Tree, Classifier
+    
+    model:Classifier = None
+    while True:
+        try: 
+            userChoice = int(input("Choose between the two classification models: kNN(1) or Decision Tree(2)").strip())
+            match userChoice:
+                case 1:
+                    model = Classifier_kNN(data)
+                    break
+                case 2:
+                    model = Classifier_Decision_Tree(data)
+                    break
+                case _:
+                    print("Invalid Input")   
+        except:
+             print("Invalid input")
+    model.evaluate()
+   
+        
+            
+        
+       
+        
+        
     
 
 
