1aafd96c0fb7c398c15618d9685738bd1b28c472
RonniEvrensel <ronni94@live.se>
2025-10-17 16:29:47 +0200

    Created abstract class Classifier, class Classifier_kNN, and class Classifier_Decision_Tree.

--- src/classifiers.py
+++ src/classifiers.py
@@ -3,13 +3,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.tree import DecisionTreeClassifier
 from data import Data
 
 
-class Classifier_kNN:
+class Classifier(ABC):
+    def __init__(self, data: Data):
+        super().__init__(self, data)
+        self.data = data
+
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+
+class Classifier_kNN(Classifier):
 
     def __init__(self, data: Data):
-        self.data = data
+        super().__init__(data=data)
 
         self.knn = KNeighborsClassifier(n_neighbors=1)
 
@@ -19,5 +30,20 @@ class Classifier_kNN:
 
     # en metod för Evaluate som printar accuracy och classification report
     def evaluate(self):
-        accuracy = accuracy_score(self, )
-        report = classification_report(self, )
+        print("Accuracy:", accuracy_score(self.data.classes_test, self.predictions))
+        print(classification_report(self.data.classes_test, self.predictions))
+
+
+class Classifier_Decision_Tree(Classifier):
+    def _init_(self, data: Data):
+        super().__init__(data=data)
+        self.dt = DecisionTreeClassifier()
+        self.dt.fit(self.data.features_train, self.data.classes_train)
+
+        self.predictions = self.dt.predict(self.data.features_test)
+
+    # en metod för Evaluate som printar accuracy och classification report
+    def evaluate(self):
+        print("Accuracy:", accuracy_score(self.data.classes_test, self.predictions))
+        print(classification_report(self.data.classes_test, self.predictions))
+
